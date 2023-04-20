3607c18636777887ca4134f8cb00bba38f3054bf
fai <s10243483@connect.np.edu.sg>
2023-04-20 13:19:48 +0800

    1st

--- Question4.java
+++ Question4.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
 
-public class Question4
-{
-  public static void main(String[] args)
-  {
+public class Question4 {
+  public static void main(String[] args) {
     /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
+     * Prints a right angle triangle with *. The base of the triangle depends on the
+     * integer given.
+     * e.g.
+     * > 5
+     * *****
+     * ****
+     * ***
+     * **
+     * *
      * 
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Please enter a number: ");
+    int num = in.nextInt();
+    int a, b;
+    for (a = num; a >= 1; a--) {
+      for (b = 1; b <= a; b++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
   }
 }
--- Question5.java
+++ Question5.java
@@ -1,32 +1,53 @@
 import java.util.Scanner;
 
-public class Question5
-{
-  public static void main(String[] args)
-  {
+public class Question5 {
+  public static void main(String[] args) {
     /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
+     * Prompt the user for number. This input indicates the number of integers the
+     * user will be entering next.
+     * Print out the mode (highest occurrence) from the set of integers.
+     * e.g.
+     * > 5
+     * > 2
+     * > 4
+     * > 1
+     * > 3
+     * > 4
+     * 4
      * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
+     * e.g.
+     * > 4
+     * > 2
+     * > 2
+     * > 3
+     * > 3
+     * 2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter the number of integars: ");
+    int num = in.nextInt();
+    int[] numarr = new int[num];
+    int maxCount = 0;
+    int mode = 0;
+    for (int i = 0; i < num; i++) {
+      System.out.print("Enter integar: ");
+      numarr[i] = in.nextInt();
+    }
+
+    for (int i = 0; i < num; i++) {
+      int count = 0;
+      for (int j = 0; j < num; j++) {
+        if (numarr[j] == numarr[i]) {
+          count++;
+        }
+      }
+      if (count > maxCount) {
+        maxCount = count;
+        mode = numarr[i];
+      }
+    }
+    System.out.print("Mode: " + mode);
   }
 }
